92c8d5c1688811ddad5b3d6ef423e09c5c71683d
cuber-lotus <1539349804@qq.com>
2024-01-20 11:59:57 +0800

    软件设计原则 1小时系列 （C++版）

--- other/bobo/code/CRP/new.cpp
+++ other/bobo/code/CRP/new.cpp
@@ -4,7 +4,7 @@
 class A {
 protected:
     std::string name;
-    int age;
+    int         age;
 
 public:
     void methodA() {
@@ -12,15 +12,17 @@ public:
     }
 };
 
+// 继承转组合
 class B {
 private:
+    // 不用知道a的具体细节
     A a;
 
 public:
     void methodB() {
         std::cout << "B " << __func__ << std::endl;
     }
-
+    // 可以直接转发，也可以添加额外操作
     void methodA() {
         a.methodA();
     }
